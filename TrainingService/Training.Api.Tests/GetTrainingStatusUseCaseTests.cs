84b5db0d4651d927b7257ab59c426dac8b8093b1
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2026-04-12 17:46:24 -0400

    Add sample unit test project

---
+++ TrainingService/Training.Api.Tests/GetTrainingStatusUseCaseTests.cs
@@ -0,0 +1,17 @@
+﻿using Training.Application.UseCases;
+
+namespace Training.Api.Tests;
+
+public sealed class GetTrainingStatusUseCaseTests
+{
+    [Fact]
+    public void Execute_ReturnsOkTrainingStatusResponse()
+    {
+        var useCase = new GetTrainingStatusUseCase();
+
+        var response = useCase.Execute();
+
+        Assert.Equal("Training.Api", response.Service);
+        Assert.Equal("ok", response.Status);
+    }
+}
